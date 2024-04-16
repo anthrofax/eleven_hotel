53e63028dd449d6f3dd63db8fbab7dc65738f5e0
anthrofax <afridhoikhsan@gmail.com>
2024-04-16 23:24:12 +0700

    Intialize project

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import { BiMessageDetail } from "react-icons/bi";
+import { BsFillSendFill, BsTelephoneOutbound } from "react-icons/bs";
+
+function Footer() {
+  return (
+    <footer className="mt-16 ">
+      <div className="container mx-auto px-4 ">
+        <Link href="/" className="font-black text-tertiary-dark">
+          Eleven Hotel
+        </Link>
+
+        <h4 className="font-semibold text-[40px] py-6">Contact</h4>
+
+        <div className="flex flex-wrap gap-16 items-center justify-between">
+          <div className="flex-1">
+            <p>123 Road</p>
+            <div className="flex items-center py-4">
+              <BsFillSendFill />
+              <p className="ml-2">codewithlari</p>
+            </div>
+            <div className="flex items-center ">
+              <BsTelephoneOutbound />
+              <p className="ml-2">000-000-00</p>
+            </div>
+            <div className="flex items-center pt-4">
+              <BiMessageDetail />
+              <p className="ml-2">codewithlari</p>
+            </div>
+          </div>
+
+          <div className="flex-1 md:text-right">
+            <p className="pb-4">Out Story</p>
+            <p className="pb-4">Get in touch</p>
+            <p className="pb-4">Out Privacy Commitment</p>
+            <p className="pb-4">Terms of service</p>
+            <p className="pb-4">Customer of Assistance</p>
+          </div>
+
+          <div className="flex-1 md:text-right">
+            <p className="pb-4">Dining Experience</p>
+            <p className="pb-4">Wellness</p>
+            <p className="pb-4">Fitness</p>
+            <p className="pb-4">Sports</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="bg-tertiary-light h-10 md:h-[70px] mt-16 w-full bottom-0 left-0"></div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/context/themeContext.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+} from "react";
+
+type ThemeContextType = {
+  darkTheme: boolean;
+  setDarkTheme: Dispatch<SetStateAction<boolean>>;
+};
+
+const ThemeContext = createContext<ThemeContextType>({
+  darkTheme: false,
+  setDarkTheme: () => null,
+});
+
+function ThemeContextProvider({ children }: { children: React.ReactNode }) {
+  const themeFromStorage: boolean =
+    typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme")
+      ? JSON.parse(localStorage.getItem("hotel-theme")!)
+      : false;
+
+  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+  const [renderComponent, setRenderComponent] = useState(false);
+
+  useEffect(function () {
+    setRenderComponent(true);
+  }, []);
+
+  if (!renderComponent) return <></>;
+
+  return (
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+        <div className="dark:bg-black dark:text-white text-[#1E1E1E]">
+          {children}
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+function useThemeContext() {
+  const context = useContext(ThemeContext);
+  console.log(context)
+
+  if (!context) alert("Anda menggunakan context di luar jangkauan");
+
+  return context;
+}
+
+export { ThemeContextProvider, useThemeContext };
