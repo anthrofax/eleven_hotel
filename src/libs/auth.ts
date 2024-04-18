8e5e195db4e8c222b9db29cc12d68f2793fcd9ea
anthrofax <afridhoikhsan@gmail.com>
2024-04-18 12:52:05 +0700

    Initialize auth options and next-auth handler

---
+++ src/libs/auth.ts
@@ -0,0 +1,26 @@
+import { NextAuthOptions } from "next-auth";
+import { SanityAdapter, SanityCredentials } from "next-auth-sanity";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import sanityClient from "./sanity";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    SanityCredentials(sanityClient),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  adapter: SanityAdapter(sanityClient),
+  debug: process.env.NODE_ENV === "development",
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {}
+};
