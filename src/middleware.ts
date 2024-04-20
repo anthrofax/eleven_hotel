138139b32c0e6a3ead7d58bc3d0afd5b7e7c1dc8
anthrofax <afridhoikhsan@gmail.com>
2024-04-20 22:27:11 +0700

    Adding middleware to block unauthenticated user's request to visit /users/:path* & /api/:path* url

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+// Middleware bawaan next-auth untuk memblokir request jika user blum login
+export { default } from "next-auth/middleware";
+
+// Path mana middleware akan diterapkan
+export const config = {
+  matcher: ["/users/:path*", "/api/:path*"], // Jika user mengunjungi path tersebut, dengan menggunakan "default" middleware, request user tidak akan diproses lebih lanjut
+};
